979fbd60a74b08a10b5efdb881468e1af37ddec3
Shubhshekhar99 <shubhshekhar99@gmail.com>
2026-01-02 02:07:50 +0530

    Configure S3 backend with DynamoDB locking and infra setup

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "cn-web-terraform-state"
     key            = "terraform.tfstate"
     region         = "us-west-1"
-    # Replace dynamodb_table with:
-    use_lockfile   = true
+    dynamodb_table = "terraform-locks"
+    encrypt   = true
   }
 }
